ceca67b06df0c43a4c47884470b863a034298b5b
Anand <urajmainali5@gmail.com>
2020-01-18 14:12:06 +0545

    composer.json file updated

--- src/config/package.php
+++ src/config/package.php
@@ -1,15 +1,26 @@
 <?php
 
 return [
-
+  /**
+   * Main folder to holds all the packages. Contains packages with VendorName. 
+   */
   'path' => 'packages',
-
+  
+  /**
+   * The VendorName will be generated inside the main folder.
+   */
   'vendorName' => "VendorName",
-
+  
+  /**
+   * These are the folders that will be generated while creating package.
+   */
   'folders' => [
     'controllers', 'databases/migrations', 'models', 'policies', 'resources/views', 'routes'
   ],
-
+  
+  /**
+   * These are the files that will be generated while creating package.
+   */
   'files' => ['routes/web']
 
 ];
